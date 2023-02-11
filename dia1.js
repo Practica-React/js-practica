ac2da2cc1760aeea443d1713efb280be13a07524
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-02-11 19:48:32 -0300

    Create dia1.js

---
+++ dia1.js
@@ -0,0 +1,4 @@
+// Actividad:
+// 1-Crear la constante alumnos que sea una array con 5 estudiantes con las propiedades nombre y edad ,3 deben ser mayores de edad 2 menores
+// 2-filter para filtrar los mayores
+// 3-devolver la actividad 2
